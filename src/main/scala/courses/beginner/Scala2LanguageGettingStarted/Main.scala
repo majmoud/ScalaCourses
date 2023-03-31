b30c3b3599e269ece0879f0874d4ef8d390944ba
majmoud <majtdsi@gmail.com>
2023-03-31 16:39:14 +0200

    Indentation de Main

--- src/main/scala/courses/beginner/Scala2LanguageGettingStarted/Main.scala
+++ src/main/scala/courses/beginner/Scala2LanguageGettingStarted/Main.scala
@@ -2,31 +2,31 @@ package courses.beginner.Scala2LanguageGettingStarted
 
 object Main {
 
-def main(args: Array[String]): Unit = {
-        // money 100
+        def main(args: Array[String]): Unit = {
+                // money 100
 
-        val Array(code : String, value : String) = args(0).split("\\s")
-        val valueAsDouble = value.toDouble
+                val Array(code : String, value : String) = args(0).split("\\s")
+                val valueAsDouble = value.toDouble
 
-        if (! Set("NZD", "CAD").contains(code)) {
-                println("Supported Currencies are CAD and NZD")
-                sys.exit()
+                if (! Set("NZD", "CAD").contains(code)) {
+                        println("Supported Currencies are CAD and NZD")
+                        sys.exit()
                 }
                 else {
-                val rate = Utils.getRateFromCode(code)
-                println(args(0) + " = USD " + (rate * valueAsDouble) )
+                        val rate = Utils.getRateFromCode(code)
+                        println(args(0) + " = USD " + (rate * valueAsDouble) )
                 }
         }
 
-object Utils {
+        object Utils {
 
-        private val cadToUSD : Double = 1 / 1.30
-        private val nzdToUSD : Double = 1 / 1.50
+                private val cadToUSD : Double = 1 / 1.30
+                private val nzdToUSD : Double = 1 / 1.50
 
-        def getRateFromCode(code: String): Double = {
-                println(s"Getting rate from ${code}")
-                code match {
-                        case "NZD" => nzdToUSD
+                def getRateFromCode(code: String): Double = {
+                        println(s"Getting rate from ${code}")
+                        code match {
+                                case "NZD" => nzdToUSD
                                 case "CAD" => cadToUSD
                         }
                 }
